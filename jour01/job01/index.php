0860ccd5487df991a11a2002a21c616d32136774
feriale-bourega <feriale-bourega@laplaterforme.io>
2021-11-02 13:55:21 +0100

    job01 FINI

---
+++ jour01/job01/index.php
@@ -0,0 +1,16 @@
+<?php
+$str = "LAPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+echo 'Vive '.$str.' !'."<br>";
+$val = 6;
+echo $val.'+4'."<br>";
+$mybool= "true";
+echo $mybool."<br>";
+$mybool= "false";
+echo $mybool;
+
+
+
+
+?>
