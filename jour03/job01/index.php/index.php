f6b04335d6fd087cf5e0a0a93c529e6b90526717
feriale-bourega <feriale-bourega@laplaterforme.io>
2021-11-07 20:55:41 +0100

    runtrack2 jour03 fini

--- jour03/job01/index.php/index.php
+++ jour03/job01/index.php/index.php
@@ -1,28 +1,17 @@
+
 <?php
-//construction du tableau de nombres
-$nombre=array('200','204','173', '98', '171', '404', '459');
- 
-//parcours du tableau
- 
+    $tableau = [200, 204, 173, 98, 171, 404, 459];
 
-/*
-Pour chaque valeur du tableau $nombre, compose la variable $verdict et affiche le verdict puis va à la ligne...
-*/
- function parite($nombre){
-    //si le reste de la division est zéro, c'est pair
-    if (($nombre%2)==0){
-        //on initialise les deux valeurs de verdict
-        $verdict='pair';
+    foreach($tableau as $element) {
+       // echo $element.'</br>';
+       if($element %2 == 0) {
+            echo $element." est paire </br>";
+       }
+       else {
+        echo $element." est impaire </br>";
+       }
     }
-    else{
-        $verdict='impair';
-    }
-    //on renvoie le verdict, tout à la fin
-    return $verdict;
-}
 
-foreach($nombre as $verdict){
-    echo'- '.parite($nombre).'<br/>';
-}
-
+         
 ?>
+ 
