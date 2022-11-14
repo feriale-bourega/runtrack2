a08ae673d37095c7bea693804bf80e505b88c87d
feriale-bourega <92152060+feriale-bourega@users.noreply.github.com>
2022-11-14 14:21:31 +0100

    Add files via upload

---
+++ jour01/job02/index.php/index.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Hello LaPlateforme!";
+/* $name contient la chaîne de caractères Hello LaPlateforme*/
+echo $name."<br>";
+
+$name = "Hello LaPlateforme!";
+/* $name est le nom de la variable*/
+echo $name;
+
+
+?>
---
+++ jour01/job03/index.php/index.php
@@ -0,0 +1,43 @@
+
+<?php
+$vrai = true;
+$faux = false;
+echo 'les variables booléennes sont '.$vrai. ' et '.$faux."<br>";
+$prez = "Je m'appelle Pierre";
+            $age = 28; //Stocke le nombre 28
+            $age2 = "28"; //Stocke la chaine de caractères "28"
+            $distance = 2.84;
+            echo $prez." et j'ai ".$age." ans. "."Je mesure ".$distance." mètres de hauteur.";
+
+    echo "<table border=1    style='background-color: orange';  width=250 height=250>
+          <tr>
+          <td><strong>TYPE</strong></td>
+          <td><strong>NOM</strong></td>
+          <td><strong>VALEUR</strong></td>
+          </tr>
+          <tr>
+          <td>booléenne</td>
+          <td>vrai</td>
+          <td>true</td>
+          </tr>
+          <tr>
+          <td>booléenne</td>
+          <td>faux</td>
+          <td>false</td>
+          </tr>
+          <tr>
+          <td>chaîne de caractères</td>
+          <td>prez</td>
+          <td>Je m'appelle Pierre</td>
+          </tr>
+          <tr>
+          <td>nombre entier</td>
+          <td>age</td>
+          <td>28</td>
+          </tr>
+          <tr>
+          <td>nombre à  virgule flottant</td>
+          <td>distance</td>
+          <td>2.84</td>
+          </tr>";
+?>
