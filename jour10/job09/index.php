225e0d2288445c1686b0f28fec8d3ff2bbb25a07
feriale-bourega <92152060+feriale-bourega@users.noreply.github.com>
2022-11-30 17:10:58 +0100

    Add files via upload

---
+++ jour10/job09/index.php
@@ -0,0 +1,49 @@
+<?php
+/* Connexion à la base de données */
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$base = 'jour09';
+$conn = mysqli_connect($servername, $username, $password, $base);
+
+/* Requête sql qui récupère l'ensemble des informations de la table 'etudiants'*/
+$query = mysqli_query($conn, "SELECT * FROM salles ORDER BY capacite DESC") ;
+/*On récupère l'ensemble des informations de la table 'etudiants'*/
+$result= mysqli_fetch_all($query);
+var_dump($result);
+
+?>
+<!DOCTYPE HTML>
+    <html>
+       <head>
+          <title>Salles</title>
+          <meta charset="UTF-8">
+          <link rel="stylesheet" type="text/css" href="">
+       </head>
+
+<body>
+    
+  <table>
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>nom</th>
+        <th>id-etage</th>
+        <th>capacité</th>
+       </tr>
+      </thead>
+    <tbody>
+            <?php foreach ($result as $key ) { // creation d'une boucle foreach qui va inspecter tous les $key qui se trouvent dans $result
+                echo "<tr>"; // à chaque fois que la boucle va tomber sur un $key il va afficher une ligne
+                foreach ($key as $value) { // creation d'une boucle qui va inspecter les éléments $value dans $key
+                   echo "<td>$value</td>"; // à chaque fois que la boucle va tomber sur $value il va l'afficher dans un <td>
+                }
+                echo "</tr>"; // on ferme le tableau
+            } ?>      
+           
+    </tbody>
+    </table>
+    
+</body>
+</html>
---
+++ jour10/job11/index.php
@@ -0,0 +1,46 @@
+<?php
+/* Connexion à la base de données */
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$base = 'jour09';
+$conn = mysqli_connect($servername, $username, $password, $base);
+
+/* Requête sql qui récupère l'ensemble des informations de la table 'etudiants'*/
+$query = mysqli_query($conn, "SELECT AVG(capacite) FROM salles") ;
+/*On récupère l'ensemble des informations de la table 'etudiants'*/
+$result= mysqli_fetch_all($query);
+var_dump($result);
+
+?>
+<!DOCTYPE HTML>
+    <html>
+       <head>
+          <title>Salles</title>
+          <meta charset="UTF-8">
+          <link rel="stylesheet" type="text/css" href="">
+       </head>
+
+<body>
+    
+  <table>
+    <thead>
+      <tr>
+        <th>capacité moyenne</th>
+       </tr>
+      </thead>
+    <tbody>
+            <?php foreach ($result as $key ) { // creation d'une boucle foreach qui va inspecter tous les $key qui se trouvent dans $result
+                echo "<tr>"; // à chaque fois que la boucle va tomber sur un $key il va afficher une ligne
+                foreach ($key as $value) { // creation d'une boucle qui va inspecter les éléments $value dans $key
+                   echo "<td>$value</td>"; // à chaque fois que la boucle va tomber sur $value il va l'afficher dans un <td>
+                }
+                echo "</tr>"; // on ferme le tableau
+            } ?>      
+           
+    </tbody>
+    </table>
+    
+</body>
+</html>
